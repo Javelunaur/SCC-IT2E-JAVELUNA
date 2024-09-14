0a0cb90bdfc09f1111776268c70ac0c8b6482e6d
Javelunaur <jjwymeth@gmail.com>
2024-09-14 18:30:55 +0800

    Updated

--- src/javelunae/Product.java
+++ src/javelunae/Product.java
@@ -6,22 +6,22 @@ import java.util.Scanner;
 public class Product {
     public void inputProd(){
        Products[] pr = new Products[100];
-       Scanner sc = new Scanner(System.in);
+       Scanner in = new Scanner(System.in);
         System.out.print("Enter no. of Products: ");
         int np= sc.nextInt();
         
         for (int i = 0;i < np; i++){
             System.out.println("Enter details of product "+(i+1));
             System.out.print("ID: ");
-            int id = sc.nextInt();
+            int id = in.nextInt();
             System.out.print("Name: ");
-            String name= sc.next();
+            String name= in.next();
             System.out.print("Price: ");
-            double price = sc.nextDouble();
+            double price = in.nextDouble();
             System.out.print("Stocks: ");
-            int stocks = sc.nextInt();
+            int stocks = in.nextInt();
             System.out.print("Sold: ");
-            int sold = sc.nextInt();
+            int sold = in.nextInt();
             
             pr[i] = new Products();
             pr[i].addp(id, name, price, stocks, sold);        
@@ -30,13 +30,12 @@ public class Product {
         System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10s %-20s %-10s\n","ID","Name","Price"," Stocks"
         ,"Sold","Profit","Estimated Profit","Status");
         
-        
         double tpg = 0;
         for(int i = 0; i < np; i++){
         tpg = tpg + (pr[i].pr * pr[i].sol);
         pr[i].viewp();
     }
-       System.out.printf("\nGross Profit: "+tpg);
+       System.out.printf("\nTotal Gross Profit: "+tpg);
       
     }
 }
